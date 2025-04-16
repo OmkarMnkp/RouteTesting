ac8502cb8ea5173e41af76db16d5cfc2709ae3c8
unknown <omkar.omkarmankape4065@gmail.com>
2025-04-16 09:47:12 +0530

    my commit

---
+++ index.js
@@ -0,0 +1,62 @@
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+// http://localhost:8000/
+const PORT = 8000;
+
+// array 
+const products = [
+    {id:1,name:'T-shirt',price:'300',color:'red'},
+    {id:2,name:'Cargo',price:'500',color:'grey'},
+    {id:3,name:'hoodie',price:'1200',color:'yellow'},
+
+]
+
+app.get('/getallproducts',(req,res)=>{
+    res.status(200).send({
+        message:'all products fetched',
+        products
+    })
+})
+
+app.get('/',(req,res)=>{
+    res.status(200).send({message:'server running succesfully',success:true,products} )
+})
+
+app.post('/post',(req,res)=>{
+    const newProduct = req.body;
+    products.push(newProduct);
+    res.status(200).send({message:'product added succesfully',success:true})
+})
+
+app.put('/put/:id',(req,res)=>{
+    const ID = req.params.id;
+    index =products.findIndex(product => product.id == ID);
+    if(index== -1){
+        res.status(200).send({message:'product not found',success:false})
+    }
+    products[index].name = req.body.name;
+    products[index].price = req.body.price;
+    products[index].color = req.body.color;
+    res.status(200).send({message:'product updated succesfully',success:true} )
+
+})
+
+app.delete('/delete/:id',(req,res)=>{
+    const ID = req.params.id;
+    index = products.findIndex(product => product.id == ID);
+    if(index == -1){
+        res.status(200).send({message:'product not found',success :false})
+    }
+    products.splice(index,1);
+    res.status(200).send({message:'producted deleted succesfully',success:true})
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`listening on port :${PORT} `)
+})
